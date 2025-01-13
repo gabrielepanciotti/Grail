57fe6f28a48f72324974d44cda3464d9c3c3df92
Gabriele Panciotti <gabrielepanciotti@gmail.com>
2025-01-13 14:58:38 +0100

    modifica

--- 1_reduce_data.py
+++ 1_reduce_data.py
@@ -56,7 +56,7 @@ for method, reducer in reduction_methods.items():
         reduced_test, _, reduction_time_test, compression_ratio_test, _ = reducer(test_data.values, kmeans_model=kmeans_model)
         reduced_label_train, reduced_label_test = train_labels, test_labels
 
-    if reduced_train.shape[0] != len(reduced_label_train):
+    if len(reduced_train) != len(reduced_label_train):
         raise ValueError(f"Mismatch tra dati ridotti ({reduced_train.shape[0]}) ed etichette ({len(train_labels)}).")
 
     # Converti eventuali tensori in NumPy
