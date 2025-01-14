50c74563bed089a197146097cc1847bddd72aa96
Gabriele Panciotti <gabrielepanciotti@gmail.com>
2025-01-14 09:51:48 +0100

    modifica

--- config/constants.py
+++ config/constants.py
@@ -1,11 +1,11 @@
 # Percorso al file del dataset
-file_path = 'dataset/dataset_1_merged_withTarget.csv'
-#file_path = 'dataset/reduced_dataset.csv'
+#file_path = 'dataset/dataset_1_merged_withTarget.csv'
+file_path = 'dataset/reduced_dataset.csv'
 # Percorso al file del dataset
-train_file = 'dataset/dataset_1_merged_withTarget.csv'
-#train_file = 'dataset/reduced_dataset.csv'
-test_file = 'dataset/dataset_test_merged_withTarget.csv'
-#test_file = 'dataset/reduced_dataset_test.csv'
+#train_file = 'dataset/dataset_1_merged_withTarget.csv'
+train_file = 'dataset/reduced_dataset.csv'
+#test_file = 'dataset/dataset_test_merged_withTarget.csv'
+test_file = 'dataset/reduced_dataset_test.csv'
 batch_size = 32
 latent_dim = 16
 pca_components = 50  # Numero di componenti PCA
